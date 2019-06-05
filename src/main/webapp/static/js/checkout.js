18138e2e4b3557d86153da31dabe9d581b9cb977
Dávid Szabó <drszabodavid@gmail.com>
2019-06-05 17:03:20 +0200

    closable payment methods added

--- src/main/webapp/static/js/checkout.js
+++ src/main/webapp/static/js/checkout.js
@@ -1,3 +1,18 @@
 $( "#toggleform" ).on( "click", function() {
     $('#hideform').toggle();
 });
+
+$( "#creditcardform" ).on( "click", function() {
+    $('#creditcardhideform').toggle();
+    $('#paypalform').toggle();
+
+});
+
+$( "#paypalform" ).on( "click", function() {
+    $('#paypalhideform').toggle();
+    $('#creditcardform').toggle();
+});
+
+$( "#toggleDetails" ).on( "click", function() {
+    $('#detailsHideFom').toggle();
+});
