f92161e2fd95a690f021c6b3026338d2ccbb5855
kincsojerne <voros.kincsojerne@gmail.com>
2019-06-05 10:40:40 +0200

    CartDaoMem filled up

--- src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -1,4 +1,33 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.model.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static sun.net.ftp.FtpReplyCode.find;
+
 public class CartDaoMem {
+
+        private Map<Product, Integer> cartData = new HashMap<>();
+        private static CartDaoMem instance = null;
+
+        /* A private Constructor prevents any other class from instantiating.
+         */
+        private CartDaoMem() {
+        }
+
+        public static CartDaoMem getInstance() {
+            if (instance == null) {
+                instance = new CartDaoMem();
+            }
+            return instance;
+        }
+
+
+        public Map<Product, Integer> getAll() {
+            return cartData;
+        }
+
+
 }
